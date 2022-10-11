567f2eb5517b3d34922595907e166212096bc881
OctoRocket <xteamxeno@gmail.com>
2022-10-11 17:01:46 -0500

    hosting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use std::path::{Path, PathBuf};
+use rocket::{fs::NamedFile, response::Redirect};
+
+
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to(uri!("/index.html"))
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("www/").join(file)).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![files])
 }
